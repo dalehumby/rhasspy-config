60404abae6f2542c3db369b54c926ef8cdbd3290
Dale Humby <dale@humby.co.za>
2023-04-07 14:14:36 +0000

    Keep transcript as Whisper sent to us, do not lower case or remove punctuation.

--- en/whisper.sh
+++ en/whisper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# First argument is OpenAi API key
+# First argument is OpenAI API key
 
 # WAV data is avaiable via STDIN
 wav_file=/profiles/en/speech.wav
@@ -12,9 +12,6 @@ curl https://api.openai.com/v1/audio/transcriptions \
   -H "Content-Type: multipart/form-data" \
   -F file=@$wav_file \
   -F model=whisper-1 \
-  -F response_format=text \
-  | sed -e "s/[[:punct:]]//g" | sed -e "s/\(.*\)/\L\1/"
-
-# Remove puncutation and make lower case
+  -F response_format=text
 
 # Transcription on stdout
